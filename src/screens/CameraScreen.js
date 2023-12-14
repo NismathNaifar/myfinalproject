78768aee49ee2ed85f3d2a19cc3542607eccab3f
Naifar_Nismath <naifarnismath@gmail.com>
2023-12-13 16:00:35 -0800

    edit comment

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Image, Alert, StyleSheet } from 'react-native';
-import { FontAwesome5 } from '@expo/vector-icons'; // Import FontAwesome5 from @expo/vector-icons
+import { FontAwesome5 } from '@expo/vector-icons'; 
 import * as ImagePicker from 'expo-image-picker';
 
 const CameraScreen = () => {
